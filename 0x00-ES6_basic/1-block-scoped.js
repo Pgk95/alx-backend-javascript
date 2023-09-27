653456978388b51aa9d632ba6486dbc3a7236d8d
Pgk95 <117736725+Pgk95@users.noreply.github.com>
2023-09-27 13:39:25 +0300

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,8 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-	if (trueOrFalse) {
-		const task = true;
-		const task2 = false;
-	}
+  const task = false;
+  const task2 = true;
 
-	return [task, task2];
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
+  }
+
+  return [task, task2];
 }
