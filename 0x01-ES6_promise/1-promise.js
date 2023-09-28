33f2cf33045fa42f6da9e2379783379a5ee05a45
Pgk95 <117736725+Pgk95@users.noreply.github.com>
2023-09-28 12:43:46 +0300

    Update 1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 export default function getFullResponseFromAPI(success) {
-  returnnew Promise((reslove, reject) => {
+  return new Promise((reslove, reject) => {
     if (success) {
       resolve({ status: 200, body: 'Success' });
     } else {
