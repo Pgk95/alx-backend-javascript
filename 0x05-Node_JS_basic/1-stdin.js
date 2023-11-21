f318142f8140cd684ebead6c9c4823fb13d196e3
pgk95 <kalupgk@gmail.com>
2023-11-21 12:12:18 +0300

    task 1

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,18 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+
+  if (name) {
+    process.stdout.write(`Your name is: ${name}\n`);
+  } else {
+    process.stdout.write('Your name is: INPUT\n');
+  }
+
+  process.stdout.write('This important software is now closing\n');
+  process.exit();
+});
+
+process.on('exit', () => {
+  process.stdout.write('\n')
+});
