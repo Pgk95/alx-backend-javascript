f55665ee9b2ba941a3f9f2361080f98b892accd6
pgk95 <kalupgk@gmail.com>
2023-11-23 22:28:31 +0300

    quick fixtask 2

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -3,24 +3,22 @@ const expect = chai.expect;
 const calculateNumber = require("./1-calcul");
 
 describe("calculateNumber()", function () {
-    describe('calculateNumber()', () => {
-        it('should return the sum of two numbers', () => {
-            expect(calculateNumber('SUM', 1, 2)).to.equal(3);
-        });
+    it('should return the sum of two numbers', () => {
+        expect(calculateNumber('SUM', 1, 2)).to.equal(3);
+    });
 
-        it('should return the difference of two numbers', () => {
-            expect(calculateNumber('SUBTRACT', 1.4, 2.2)).to.equal(-1);
-        });
+    it('should return the difference of two numbers', () => {
+        expect(calculateNumber('SUBTRACT', 1.4, 2.2)).to.equal(-1);
+    });
 
-        it('should return the quotient of two numbers', () => {
-            expect(calculateNumber('DIVIDE', 4, 2)).to.equal(2);
-        });
-        it('should return an error message when dividing by zero', () => {
-            expect(calculateNumber('DIVIDE', 1.7, 0)).to.equal('Error');
-        });
+    it('should return the quotient of two numbers', () => {
+        expect(calculateNumber('DIVIDE', 4, 2)).to.equal(2);
+    });
+    it('should return an error message when dividing by zero', () => {
+        expect(calculateNumber('DIVIDE', 1.7, 0)).to.equal('Error');
+    });
 
-        it('should return an error message when dividing by zero', () => {
-            expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
-        });
+    it('should return an error message when dividing by zero', () => {
+        expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
     });
 });
